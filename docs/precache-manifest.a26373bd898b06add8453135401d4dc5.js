e00464e623f089631826fc5b9517cdf2799938e5
Phat Thaveepholcharoen <phat.t@wongnai.com>
2019-08-17 00:22:12 +0700

    Update build script

---
+++ docs/precache-manifest.a26373bd898b06add8453135401d4dc5.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b567cef58c196660ab260d1a01045b08",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8ad8150176c2e4a478c4.bundle.js"
+  },
+  {
+    "url": "runtime~main.8ad8150176c2e4a478c4.bundle.js"
+  },
+  {
+    "url": "vendors~main.8ad8150176c2e4a478c4.bundle.js"
+  }
+]);
