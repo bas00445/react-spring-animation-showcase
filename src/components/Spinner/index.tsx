76535a753e62a692e569bc97c7de02f0e5e38904
Phat Thaveepholcharoen <phat.t@wongnai.com>
2019-08-23 22:58:03 +0700

    Add Spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import styled from "styled-components";
+
+export default function Spinner() {
+  const [looping, setLooping] = React.useState(false);
+
+  const spinAnim = useSpring({
+    from: {
+      transform: `rotate(0deg)`
+    },
+    to: async next => {
+      while (looping) {
+        await next({ transform: `rotate(360deg)` });
+        await next({ transform: `rotate(0deg)` });
+      }
+    }
+  });
+
+  const onToggle = React.useCallback(() => {
+    setLooping(!looping);
+  }, [looping]);
+
+  return (
+    <div>
+      <Center style={spinAnim}>Loading</Center>
+      <button onClick={onToggle}>Toggle</button>
+    </div>
+  );
+}
+
+const Center = styled(animated.div)`
+  width: 100px;
+  height: 100px;
+  background: red;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+`;
